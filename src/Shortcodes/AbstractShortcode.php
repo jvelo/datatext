a7a3134f5d8adc2b2a1b3ee0ea4ff3bf4dd9ec32
Jérôme Velociter <jerome@velociter.fr>
2016-02-28 16:13:31 +0100

    Add license headers

--- src/Shortcodes/AbstractShortcode.php
+++ src/Shortcodes/AbstractShortcode.php
@@ -1,7 +1,13 @@
 <?php
+/*
+ * Copyright (c) 2016 Jérôme Velociter <jerome@velociter.fr>
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
 
 namespace Jvelo\Datatext\Shortcodes;
-
 
 abstract class AbstractShortcode implements Shortcode {
 
